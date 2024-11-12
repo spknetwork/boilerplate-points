45439cd3412ed8649e0318452f6e5e2352344121
Adesojisouljay <adesojiisrael@gmail.com>
2024-11-12 20:15:53 +0100

    create free acc, btc ord and one btc club

--- contollers/bitcoin.js
+++ contollers/bitcoin.js
@@ -1,12 +1,41 @@
 const bitcoinMessage = require("bitcoinjs-message");
 const axios = require("axios")
 const dhive = require('@hiveio/dhive');
+const { PrivateKey } = require('@hiveio/dhive');
+const { randomBytes } = require('crypto');
 const fs = require('fs').promises;
 require('dotenv').config();
 const client = require("./../hive/client")
 const BitcoinMachines = require('../models/BitcoinMachines');
 const User = require("../models/User.js");
 const { getBitcoinMainnetBalance, getBitcoinAddressTransactions } = require("../utils/bitcoin.js")
+
+const getPrivateKeys = (username, password) => {
+    const roles = ["owner", "active", "posting", "memo"];
+    const privKeys = {
+        owner: "",
+        active: "",
+        posting: "",
+        memo: "",
+        ownerPubkey: "",
+        activePubkey: "",
+        postingPubkey: "",
+        memoPubkey: ""
+    };
+
+    roles.forEach((role) => {
+        privKeys[role] = PrivateKey.fromLogin(username, password, role).toString();
+        privKeys[`${role}Pubkey`] = PrivateKey.from(privKeys[role]).createPublic().toString();
+    });
+
+    console.log(privKeys);
+    return privKeys;
+};
+
+const generatePassword = async (length) => {
+    const password = `P${PrivateKey.fromSeed(randomBytes(length).toString("hex")).toString()}`;
+    return password;
+};
 
 // Function to check if a BTC address holds a specific NFT or asset
 async function checkBTCMachineOwnership(address) {
@@ -46,15 +75,54 @@ function verifySignature(address, message, signature) {
     }
 }
 
+/////get hive acc keys
+const generateHiveAccountKeys = async (req, res) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({ error: "Username is required to generate keys." });
+    }
+
+    try {
+        const masterPassword = await generatePassword(63);
+        const privKeys = getPrivateKeys(username, masterPassword);
+
+        res.json({
+            success: true,
+            accountDetails: {
+                username,
+                owner: privKeys.owner,
+                active: privKeys.active,
+                posting: privKeys.posting,
+                memo: privKeys.memo,
+                masterPassword,
+                ownerPubkey: privKeys.ownerPubkey,
+                activePubkey: privKeys.activePubkey,
+                postingPubkey: privKeys.postingPubkey,
+                memoPubkey: privKeys.memoPubkey
+            }
+        });
+    } catch (error) {
+        console.error("Error generating keys:", error.message);
+        res.status(500).json({ error: "An error occurred while generating keys." });
+    }
+};
+
 // Endpoint to create a new account using claimed accounts with signature verification
 const createAccount = async (req, res) => {
-    const { username, address, message, signature } = req.body;
+    const { username, address, message, signature, accountKeys } = req.body;
 
-    if (!username || !address || !message || !signature) {
+    if (!username || !address || !message || !signature || !accountKeys) {
         return res.status(400).json({ error: 'Username, address, message, and signature are required' });
     }
 
     try {
+        const balance = await getBitcoinMainnetBalance(address);
+
+        if(balance < 0.00005) {
+            return res.status(400).json({ error: 'You must have at lease 0.00005btc to get a free account' });
+        }
+
         // Verify the Bitcoin message signature
         const isValid = verifySignature(address, message, signature);
 
@@ -62,11 +130,11 @@ const createAccount = async (req, res) => {
             return res.status(400).json({ error: 'Invalid signature' });
         }
 
-        // // Check if the BTC address has already been used
-        // const existingRecord = await BitcoinMachines.findOne({ bitcoinAddress: address });
-        // if (existingRecord) {
-        //     return res.status(400).json({ error: 'This BTC address has already been used to create an account' });
-        // }
+        // Check if the BTC address has already been used
+        const existingRecord = await BitcoinMachines.findOne({ bitcoinAddress: address });
+        if (existingRecord) {
+            return res.status(400).json({ error: 'This BTC address has already been used to create an account' });
+        }
 
         // Check if the username or BTC address has already been used
         const existingUser = await User.findOne({ 
@@ -78,22 +146,17 @@ const createAccount = async (req, res) => {
         }
 
         const ownsBTCMachine = await checkBTCMachineOwnership(address);
-        // if (!ownsBTCMachine) {
-        //      return res.status(400).json({ error: 'No Bitcoin Machine' });
-        // }
+        if (!ownsBTCMachine) {
+             return res.status(400).json({ error: 'No Bitcoin Machine found in the provided address' });
+        }
 
         const accountCreator = process.env.HIVE_ACCOUNT_CREATOR;
         const activeKey = dhive.PrivateKey.fromString(process.env.HIVE_ACCOUNT_CREATOR_ACTIVE_KEY);
 
-        const ownerKey = dhive.PrivateKey.fromLogin(username, 'owner', 'posting');
-        const activeKeyNew = dhive.PrivateKey.fromLogin(username, 'active', 'posting');
-        const postingKey = dhive.PrivateKey.fromLogin(username, 'posting', 'posting');
-        const memoKey = dhive.PrivateKey.fromLogin(username, 'memo', 'posting');
-
-        const publicOwnerKey = ownerKey.createPublic().toString();
-        const publicActiveKey = activeKeyNew.createPublic().toString();
-        const publicPostingKey = postingKey.createPublic().toString();
-        const publicMemoKey = memoKey.createPublic().toString();
+        const publicOwnerKey = accountKeys.ownerPubkey
+        const publicActiveKey = accountKeys.activePubkey
+        const publicPostingKey = accountKeys.postingPubkey
+        const publicMemoKey = accountKeys.memoPubkey
 
         const createAccount = await client.broadcast.sendOperations(
             [
@@ -126,8 +189,6 @@ const createAccount = async (req, res) => {
             activeKey
         );
 
-        console.log(createAccount)
-
         const newUser = new User({
             username,
             bitcoinAddress: address,
@@ -140,12 +201,213 @@ const createAccount = async (req, res) => {
         res.json({
             success: true,
             result: createAccount,
-            keys: {
-                owner: ownerKey.toString(),
-                active: activeKeyNew.toString(),
-                posting: postingKey.toString(),
-                memo: memoKey.toString(),
-            }
+            message: "Hive account created succesfully"
+            // keys: {
+            //     owner: ownerKey.toString(),
+            //     active: activeKeyNew.toString(),
+            //     posting: postingKey.toString(),
+            //     memo: memoKey.toString(),
+            // }
+        });
+    } catch (error) {
+        console.log("error.....", error.message)
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const createOneBtcAccount = async (req, res) => {
+    const { username, address, message, signature, accountKeys } = req.body;
+    console.log(req.body)
+
+    if (!username || !address || !message || !signature || !accountKeys) {
+        return res.status(400).json({ error: 'Username, address, message, and signature are required' });
+    }
+    
+    try {
+        const balance = await getBitcoinMainnetBalance(address);
+
+        if(balance < 0.00005) {
+            return res.status(400).json({ error: 'You must have at lease 0.00005btc to get a free account' });
+        }
+
+        // Verify the Bitcoin message signature
+        const isValid = verifySignature(address, message, signature);
+
+        if (!isValid) {
+            return res.status(400).json({ error: 'Invalid signature' });
+        }
+
+        // Check if the BTC address has already been used
+        const existingRecord = await BitcoinMachines.findOne({ bitcoinAddress: address });
+        if (existingRecord) {
+            return res.status(400).json({ error: 'This BTC address has already been used to create an account' });
+        }
+
+        // Check if the username or BTC address has already been used
+        const existingUser = await User.findOne({ 
+            $or: [{ username }, { bitcoinAddress: address }]
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ error: 'This username or BTC address has already been used to create an account' });
+        }
+
+        const accountCreator = process.env.HIVE_ACCOUNT_CREATOR;
+        const activeKey = dhive.PrivateKey.fromString(process.env.HIVE_ACCOUNT_CREATOR_ACTIVE_KEY);
+
+        // const ownerKey = dhive.PrivateKey.fromLogin(username, 'owner', 'posting');
+        // const activeKeyNew = dhive.PrivateKey.fromLogin(username, 'active', 'posting');
+        // const postingKey = dhive.PrivateKey.fromLogin(username, 'posting', 'posting');
+        // const memoKey = dhive.PrivateKey.fromLogin(username, 'memo', 'posting');
+
+        const publicOwnerKey = accountKeys.ownerPubkey
+        const publicActiveKey = accountKeys.activePubkey
+        const publicPostingKey = accountKeys.postingPubkey
+        const publicMemoKey = accountKeys.memoPubkey
+
+        const createAccount = await client.broadcast.sendOperations(
+            [
+                [
+                    'create_claimed_account',
+                    {
+                        creator: accountCreator,
+                        new_account_name: username,
+                        owner: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicOwnerKey, 1]],
+                        },
+                        active: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicActiveKey, 1]],
+                        },
+                        posting: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicPostingKey, 1]],
+                        },
+                        memo_key: publicMemoKey,
+                        json_metadata: '',
+                        extensions: [],
+                    },
+                ],
+            ],
+            activeKey
+        );
+
+        const newUser = new User({
+            username,
+            bitcoinAddress: address,
+            signature,
+        });
+
+        await newUser.save();
+
+        const newBtcUser = new BitcoinMachines({
+            username,
+            bitcoinAddress: address,
+            signature,
+        });
+        
+        newBtcUser.save();
+
+        res.json({
+            success: true,
+            result: createAccount,
+            message: "Hive account created succesfully"
+            // keys: {
+            //     owner: ownerKey.toString(),
+            //     active: activeKeyNew.toString(),
+            //     posting: postingKey.toString(),
+            //     memo: memoKey.toString(),
+            // }
+        });
+    } catch (error) {
+        console.log("error.....", error.message)
+        res.status(500).json({ error: error.message });
+    }
+};
+
+/////This should be move to the right folder or we can rename the folder to hiveAccounts
+const createFreeAccount = async (req, res) => {
+    const { username, accountKeys } = req.body;
+
+    console.log({ username, accountKeys })
+
+    if (!username ) {
+        return res.status(400).json({ error: 'Username is required' });
+    }
+    
+    try {
+
+        // Check if the username or BTC address has already been used
+        const existingUser = await User.findOne({ username });
+
+        if (existingUser) {
+            return res.status(400).json({ error: 'This username has already been used to create an account' });
+        }
+
+        const accountCreator = process.env.HIVE_ACCOUNT_CREATOR;
+        const activeKey = dhive.PrivateKey.fromString(process.env.HIVE_ACCOUNT_CREATOR_ACTIVE_KEY);
+
+        // const ownerKey = dhive.PrivateKey.fromLogin(username, 'owner', 'posting');
+        // const activeKeyNew = dhive.PrivateKey.fromLogin(username, 'active', 'posting');
+        // const postingKey = dhive.PrivateKey.fromLogin(username, 'posting', 'posting');
+        // const memoKey = dhive.PrivateKey.fromLogin(username, 'memo', 'posting');
+
+        const publicOwnerKey = accountKeys.ownerPubkey
+        const publicActiveKey = accountKeys.activePubkey
+        const publicPostingKey = accountKeys.postingPubkey
+        const publicMemoKey = accountKeys.memoPubkey
+
+        const createAccount = await client.broadcast.sendOperations(
+            [
+                [
+                    'create_claimed_account',
+                    {
+                        creator: accountCreator,
+                        new_account_name: username,
+                        owner: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicOwnerKey, 1]],
+                        },
+                        active: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicActiveKey, 1]],
+                        },
+                        posting: {
+                            weight_threshold: 1,
+                            account_auths: [],
+                            key_auths: [[publicPostingKey, 1]],
+                        },
+                        memo_key: publicMemoKey,
+                        json_metadata: '',
+                        extensions: [],
+                    },
+                ],
+            ],
+            activeKey
+        );
+
+        const newUser = new User({
+            username,
+        });
+
+        await newUser.save();
+
+        res.json({
+            success: true,
+            result: createAccount,
+            message: "Hive account created successfully"
+            // keys: {
+            //     owner: ownerKey.toString(),
+            //     active: activeKeyNew.toString(),
+            //     posting: postingKey.toString(),
+            //     memo: memoKey.toString(),
+            // }
         });
     } catch (error) {
         console.log("error.....", error.message)
@@ -181,4 +443,4 @@ const getAddressTransactions = async (req, res) => {
     }
 }
 
-module.exports = { checkBTCMachineOwnership, createAccount, checkBtcBal, getAddressTransactions };
+module.exports = { checkBTCMachineOwnership, createAccount, checkBtcBal, getAddressTransactions, createOneBtcAccount, createFreeAccount, generateHiveAccountKeys };
